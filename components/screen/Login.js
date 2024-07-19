c7313a926d82c846420f1591b0afb741f2fd55dc
Adline Samuel <adlinesamuel@Adlines-MacBook-Air-2.local>
2024-07-19 00:54:23 -0400

    Added Plugin

--- components/screen/Login.js
+++ components/screen/Login.js
@@ -19,17 +19,19 @@ const Login = () => {
     }
   return (
     <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              alt="Your Company"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              className="mx-auto h-10 w-auto"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
+        < div className="flex min-h-full flex-1 flex-col justify-center items-center px-6 py-12 pt-40 lg:px-8">
+  <div className="sm:mx-auto text-center sm:w-full sm:max-w-sm">
+    <img
+      alt=""
+      src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+      className="h-8 w-auto mx-auto"
+    />
+    <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      Sign in to your account
+    </h2>
+  </div>
+
+
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form action="#" method="POST" className="space-y-6" onSubmit={(e)=>handleSubmit(e)}>
